f4fa186f895761f2a7cafc62f9e6214d7c91aff3
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-06-17 08:00:18 -0400

    Setter tests completed

--- Assignment2/Assignment2.Tests/RectangleTest.cs
+++ Assignment2/Assignment2.Tests/RectangleTest.cs
@@ -10,6 +10,7 @@ namespace Assignment2.Tests
 {
     public class RectangleTest
     {
+        // Length getter tests
         [Test]
         public void LengthGetterWithDefaultConstructor_InputNone_Output1()
         {
@@ -56,6 +57,7 @@ namespace Assignment2.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        //Width getter tests
         [Test]
         public void WidthGetterWithDefaultConstructor_InputNone_Output1()
         {
@@ -102,6 +104,7 @@ namespace Assignment2.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        //Perimeter getter tests
         [Test]
         public void PerimeterGetterWithDefaultConstructor_InputNone_Output4()
         {
@@ -148,6 +151,7 @@ namespace Assignment2.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        // Area getter tests
         [Test]
         public void AreaGetterWithDefaultConstructor_InputNone_Output1()
         {
@@ -189,6 +193,106 @@ namespace Assignment2.Tests
 
             //Act
             int actual = rectangle.GetArea();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Length setter tesets
+        [Test]
+        public void LengthSetterWithGivenInputAndSettingWithDifferentValue_Input3Then4And8_Output4()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle(3, 8);
+            rectangle.SetLength(4);
+
+            int expected = 4;
+
+            //Act
+            int actual = rectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LengthSetterWithDefaultConstructorThenGivenInput_InputNoneThen7_Output7()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetLength(7);
+
+            int expected = 7;
+
+            //Act
+            int actual = rectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LengthSetterWithDefaultConstructorThenGivenInputThen1_InputNoneThen7then1_Output1()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetLength(7);
+            rectangle.SetLength(1);
+
+            int expected = 1;
+
+            //Act
+            int actual = rectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Width setter tests
+        [Test]
+        public void WidthSetterWithGivenInputAndSettingWithDifferentValue_Input3And5Then6_Output6()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle(3, 5);
+            rectangle.SetWidth(6);
+
+            int expected = 6;
+
+            //Act
+            int actual = rectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void WidthSetterWithDefaultConstructorThenGivenInput_InputNoneThen9_Output9()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(9);
+
+            int expected = 9;
+
+            //Act
+            int actual = rectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void WidthSetterWithDefaultConstructorThenGivenInputThen1_InputNoneThen2then1_Output1()
+        {
+            //Arrange
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(2);
+            rectangle.SetWidth(1);
+
+            int expected = 1;
+
+            //Act
+            int actual = rectangle.GetWidth();
 
             //Assert
             Assert.AreEqual(expected, actual);
